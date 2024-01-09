349a1c101e34162a093a42e628007bde990b4661
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2024-01-10 00:35:28 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Blog from './pages/Blog/Blog';
+import About from './pages/About/About';
+import Shop from './pages/Shop/Shop'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/shop" element={<Shop/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './shop.css'
+const Shop = () => {
+  return (
+    <div>Shop</div>
+  )
+}
+
+export default Shop
