347526049d2ae91e45f9c4772436a645d090c16e
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2024-01-11 18:47:25 +0500

    Navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import logo from "../../assets/imgs/logo.svg";
 import shoppingCart from "../../assets/icons/shopping-cart.svg";
-
+import { AiOutlineBars } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 import "./navbar.scss";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setTimeout(() => {
+      setMobileMenuOpen(!isMobileMenuOpen);
+    }, 100);
+  };
   return (
     <nav>
       <div className="container">
-        <div className="navbar">
+        <div className={`navbar ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}>
           <div className="navbar__left">
             <a href="/">
               {" "}
@@ -34,6 +43,36 @@ const Navbar = () => {
               </li>
             </ul>
             <button>Log in</button>
+          </div>
+
+          <div className="mobile__menu">
+            <button onClick={toggleMobileMenu} >
+              {isMobileMenuOpen ? (
+                <IoClose style={{ color: "#fff", fontSize: "27px" }} />
+              ) : (
+                <AiOutlineBars style={{ color: "#fff", fontSize: "27px" }} />
+              )}
+            </button>
+            {isMobileMenuOpen && (
+              <ul>
+                <li>
+                  <NavLink to="/" activeClassName="active">
+                    Explore
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/shop" activeClassName="active">
+                    Shop
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/blog" activeClassName="active">
+                    Blog
+                  </NavLink>
+                </li>
+                <button>Log in</button>
+              </ul>
+            )}
           </div>
         </div>
       </div>
